7b21a29726120e9c8af4334943870ec58960c491
Love Dufva <love@lovedufva.se>
2014-12-23 14:52:21 +0100

    Create buss.pl

---
+++ buss.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl -w
+
+# use module
+use XML::Simple;
+use Data::Dumper;
+use LWP::Simple;
+use URI;
+
+# create object
+my $parser      = new XML::Simple;
+my $url         = URI->new( 'http://api.sl.se/api2/realtimedepartures.xml?' );
+$url->query_form(
+        'key'           => '9e99975c83864b6f8dab6ab4baddaba8',
+        'siteid'        => $ARGV[0],
+        'timewindow'    => '10',
+);
+my $file        = "sl.xml";
+
+#get response
+my $response    = getstore($url, $file);
+my $doc         = $parser->XMLin($file);
+
+# read XML file
+#/ResponseOfDepartures/ResponseData/Buses/Bus/"buss från"..StopAreaName.."Till"..Destination.."Avgår"..DisplayTime
+foreach $e (@{$doc->{ResponseData}->{Buses}->{Bus}})
+{
+                print "Buss från ", $e->{StopAreaName}, " mot ", $e->{Destination}, " avgår ";
+                if ($e->{DisplayTime} eq 'Nu') {
+                        print "nu";
+                }
+                elsif ($e->{DisplayTime} =~ 'min') {
+                        print "om ", $e->{DisplayTime};
+                }
+                elsif ($e->{DisplayTime} =~ ':') {
+                        print $e->{DisplayTime};
+                }
+                print ".", "\n", "\n";
+}
