15102edae3de20ebd6767b7abea9d62df1fa1d07
Love Dufva <love@lovedufva.se>
2014-12-25 22:38:15 +0100

    Update tåg.pl

--- tåg.pl
+++ tåg.pl
@@ -12,21 +12,21 @@ my $url         = URI->new( 'http://api.sl.se/api2/realtimedepartures.xml?' );
 $url->query_form(
         'key'           => '9e99975c83864b6f8dab6ab4baddaba8',
         'siteid'        => $ARGV[0],
-        'timewindow'    => '10',
+        'timewindow'    => '15',
 );
 my $file        = "sl2.xml";
 
-#print $url;
-
 #get response
 my $response    = getstore($url, $file);
+$edit = `sed -i 's/å/a/g' $file`;
+$edit = `sed -i 's/ä/a/g' $file`;
+$edit = `sed -i 's/ö/o/g' $file`;
+$edit = `sed -i 's/Å/A/g' $file`;
+$edit = `sed -i 's/Ä/A/g' $file`;
+$edit = `sed -i 's/Ö/O/g' $file`;
 my $doc         = $parser->XMLin($file, ForceArray=>['Metro']);
 
-#print Dumper($doc);
-#die;
-
 # read XML file
-#/ResponseOfDepartures/ResponseData/Buses/Bus/"buss från"..StopAreaName.."Till"..Destination.."Avgår"..DisplayTime
 foreach $e (@{$doc->{ResponseData}->{Metros}->{Metro}})
 {
                 print $e->{GroupOfLine}, " ", $e->{LineNumber}, " från ", $e->{StopAreaName}, " mot ", $e->{Destination};
@@ -34,18 +34,17 @@ foreach $e (@{$doc->{ResponseData}->{Metros}->{Metro}})
                 print " avgår ";
 
                 if ($e->{DisplayTime} eq 'Nu') {
-                        print "nu";
+                        print "nu.";
                 }
 
                 elsif ($e->{DisplayTime} =~ 'min') {
-                        print "om ", $e->{DisplayTime};
+                        print "om ", $e->{DisplayTime}, ".";
                 }
 
                 elsif ($e->{DisplayTime} =~ ':') {
-                        print $e->{DisplayTime};
+                        print $e->{DisplayTime}, ".";
                 }
 
-                print ".", "\n", "\n";
 
 
 }
