22e1b5c95aca3cdcfb4581c8961ae1118b39713b
Love Dufva <love@lovedufva.se>
2014-12-29 22:02:55 +0100

    Update alt-sl.py

--- alt-sl.py
+++ alt-sl.py
@@ -8,20 +8,45 @@ import urllib2
 import sys
 
 #Vars!
-data = {}
-data['key'] = '8c9bf84d3f3746eab8568891dafe74f8'
-data['maxresults'] = '1'
-data['searchstring'] = sys.argv[1]
-url_values = urllib.urlencode(data)
-url = 'http://api.sl.se/api2/typeahead.xml'
-full_url = url + '?' + url_values
-data = urllib2.urlopen(full_url)
+uppslag_data = {}
+uppslag_data['key'] = '8c9bf84d3f3746eab8568891dafe74f8' #SL PLATSUPPSLAG API-NYCKEL
+uppslag_data['maxresults'] = '1'
+uppslag_data['searchstring'] = sys.argv[1]
+uppslag_url_values = urllib.urlencode(uppslag_data)
+uppslag_url = 'http://api.sl.se/api2/typeahead.xml'
+uppslag_full_url = uppslag_url + '?' + uppslag_url_values
+uppslag_data = urllib2.urlopen(uppslag_full_url)
 
-document = ElementTree.parse(data)
-sites = document.find('ResponseData/Site')
-
-#Output
-
-for Site in sites:
+uppslag_document = ElementTree.parse(uppslag_data)
+uppslag_sites = uppslag_document.find('ResponseData/Site')
+for Site in uppslag_sites:
         if Site.tag == 'SiteId':
                 siteid = Site.text
+
+#New vars!
+realtid_data = {}
+realtid_data['key'] = '9e99975c83864b6f8dab6ab4baddaba8'
+realtid_data['siteid'] = siteid
+realtid_data['timewindow'] = 15
+realtid_url_values = urllib.urlencode(realtid_data)
+realtid_url = 'http://api.sl.se/api2/realtimedepartures.xml'
+realtid_full_url = realtid_url + '?' + realtid_url_values
+realtid_data = urllib2.urlopen(realtid_full_url)
+realtid_document = ElementTree.parse(realtid_data)
+realtid_buses = realtid_document.find('ResponseData/Buses/Bus')
+for Bus in realtid_buses:
+        if Bus.tag == 'LineNumber':
+                linenumber = Bus.text
+        elif Bus.tag == 'Destination':
+                destination = Bus.text
+        elif Bus.tag == 'StopAreaName':
+                stopareaname = Bus.text
+        elif Bus.tag == 'DisplayTime':
+                if 'Nu' in Bus.text:
+                        displaytime = 'nu'
+                elif ':' in Bus.text:
+                        displaytime = Bus.text
+                elif 'min' in Bus.text:
+                        displaytime = 'om ' + Bus.text
+
+        print 'Buss ' + linenumber + ' från ' + stopareaname + ' mot ' + destination + ' avgår ' + displaytime
