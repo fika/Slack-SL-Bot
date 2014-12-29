d428a9af3ee61def883b4a3f23ef8c449ee23052
Love Dufva <love@lovedufva.se>
2014-12-30 00:11:39 +0100

    Update sl-json.py

--- sl-json.py
+++ sl-json.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 #Import!
-import xml.etree.cElementTree as ElementTree
 import urllib
 import urllib2
 import sys
@@ -14,16 +13,16 @@ uppslag_data['key'] = '8c9bf84d3f3746eab8568891dafe74f8' #SL PLATSUPPSLAG API-NY
 uppslag_data['maxresults'] = '1'
 uppslag_data['searchstring'] = sys.argv[1]
 uppslag_url_values = urllib.urlencode(uppslag_data)
-uppslag_url = 'http://api.sl.se/api2/typeahead.xml'
+uppslag_url = 'http://api.sl.se/api2/typeahead.json'
 uppslag_full_url = uppslag_url + '?' + uppslag_url_values
 uppslag_data = urllib2.urlopen(uppslag_full_url)
+uppslag_string = uppslag_data.read()
+uppslag_decoded = json.loads(uppslag_string)
+uppslag_ourResult = uppslag_decoded['ResponseData']
 
-uppslag_document = ElementTree.parse(uppslag_data)
-uppslag_sites = uppslag_document.find('ResponseData/Site')
-for Site in uppslag_sites:
-        if Site.tag == 'SiteId':
-                siteid = Site.text
-#print siteid
+for rs in uppslag_ourResult:
+        siteid =  rs['SiteId']
+
 #New vars!
 realtid_data = {}
 realtid_data['key'] = '9e99975c83864b6f8dab6ab4baddaba8'
@@ -35,11 +34,9 @@ realtid_full_url = realtid_url + '?' + realtid_url_values
 realtid_data = urllib2.urlopen(realtid_full_url)
 realtid_string = realtid_data.read()
 realtid_decoded = json.loads(realtid_string)
-data = json.dumps(realtid_decoded, sort_keys=True, indent=4)
-ourResult = realtid_decoded['ResponseData']['Buses']
-ppourResult = json.dumps(ourResult, sort_keys=True, indent=4)
+realtid_ourResult = realtid_decoded['ResponseData']['Buses']
 
-for rs in ourResult:
+for rs in realtid_ourResult:
         looped = u'Buss ' + rs['LineNumber'] + u' från ' + rs['StopAreaName'] + ' mot ' + rs['Destination'] + u' avgår '
         if 'Nu' in rs['DisplayTime']:
                 displaytime = 'nu'
